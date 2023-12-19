8d0431abf9a10daf971ef61c1d7d733fecffb613
gunerifurkan <92922640+gunerifurkan@users.noreply.github.com>
2023-12-19 23:49:02 +0300

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,155 +1,85 @@
 #include <iostream>
-#include <unistd.h>
-#include <stdio.h>
+#include <vector>
+#include <thread>
+#include <map>
 #include <sys/socket.h>
-#include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-#include <pthread.h>
+#include <unistd.h>
 #include <arpa/inet.h>
-#include <clocale>
-#include <sys/types.h>
-#include <netdb.h>
-#include <string>
-#include <sstream>
-#include <pthread.h>
-#include <sys/time.h>
 
+#define PORT 2023
 #define MAX_CLIENTS 7
 
 using namespace std;
 
-int clientSocket[MAX_CLIENTS] = {0};
-int serverSocket;
-struct sockaddr_in addr;
-int addrlen = sizeof(addr);
+map<int, pair<string, thread>> clientThreads;
 
-void initServer() {
-    setlocale(LC_ALL, "Turkish");
+void handleClient(int clientSocket) {
+    char buffer[1024];
 
-    int opt = 1;
+    while (true) {
+        memset(buffer, 0, 1024);
+        int read = recv(clientSocket, buffer, 1024, 0);
 
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-
-    if(serverSocket == -1) {
-        cerr << "Soket Oluşmadı! << endl;
-        exit(EXIT_FAILURE);
-    }
-
-    if(setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0) {
-        cerr << "setsockopt" << endl;
-        exit(EXIT_FAILURE);
-    }
-
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(2023);
-    inet_pton(AF_INET, "0.0.0.0", &addr.sin_addr);
-
-    if(bind(serverSocket, (sockaddr*)&addr, sizeof(addr)) < 0) {
-        cerr << "Bağlantı Hatası!" << endl;
-        exit(EXIT_FAILURE);
-    } else {
-        cout << "Bağlantı Başarılı..." << endl;
-    }
-
-    if(listen(serverSocket, MAX_CLIENTS) < 0) {
-        cerr << "Dinleme Hatası!" << endl;
-        exit(EXIT_FAILURE);
-    } else {
-        cout << "Dinleme Başarılı..." << endl;
-    }
-}
-
-void acceptConnections() {
-    int newSocket;
-    if((newSocket = accept(serverSocket, (struct sockaddr*)&addr, (socklen_t*)&addrlen)) < 0) {
-        cerr << "İstek Kabulü Hatası!" << endl;
-        exit(EXIT_FAILURE);
-    } else {
-        cout << "İstek Kabulü Başarılı... \n\n";
-    }
-
-    for(int i = 0; i < MAX_CLIENTS; i++) {
-        if(clientSocket[i] == 0) {
-            clientSocket[i] = newSocket;
+        if (read <= 0) {
+            cout << "Client disconnected or error" << endl;
+            close(clientSocket);
+            clientThreads.erase(clientSocket);
             break;
         }
-    }
-}
 
-void handleClients(fd_set readfds) {
-    char buffer[1024];
-    int bytesRecieved;
-
-    for(int i = 0; i < MAX_CLIENTS; i++) {
-        int sd = clientSocket[i];
-        if(FD_ISSET(sd, &readfds)) {
-            bytesRecieved = recv(sd, buffer, 1024, 0);
-
-            if(bytesRecieved <= 0) {
-                if(bytesRecieved == -1) {
-                    cerr << "Veri Alma Hatası!" << endl;
-                } else {
-                    cout << "Bağlantı Kesildi!" << endl;
-                }
-                close(sd);
-                clientSocket[i] = 0;
-            } else {
-                int exclude = sd;
-                cout << string(buffer, 0, bytesRecieved) << endl;
-
-                ostringstream ss;
-                ss << buffer << "\r\n";
-                string strOut = ss.str();
-
-                for(int j = 0; j < MAX_CLIENTS; j++) {
-                    sd = clientSocket[j];
-                    if(sd != serverSocket && sd != 0 && exclude != sd) {
-                        send(sd, strOut.c_str(), strOut.size() + 1, 0);
-                        FD_CLR(sd, &readfds);
-                    }
-                }
+        for (auto const& pair : clientThreads) {
+            if (pair.first != clientSocket) {
+                send(pair.first, buffer, strlen(buffer), 0);
             }
         }
     }
 }
 
-int main() {
-    initServer();
+void startServer() {
+    int serverFd, newSocket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
 
-    struct timeval interval;
-    interval.tv_sec=0.2;
-    interval.tv_usec=0;
-
-    fd_set readfds;
-
-    while(1) {
-        FD_ZERO(&readfds);
-
-        int max_sd = serverSocket;
-
-        FD_SET(serverSocket, &readfds);
-        for(int i = 0; i < MAX_CLIENTS; i++) {
-            int sd = clientSocket[i];
-
-            if(sd > 0)
-                FD_SET(sd, &readfds);
-
-            if(sd > max_sd)
-                max_sd = sd;
-        }
-
-        int activity = select(max_sd + 1, &readfds, NULL, NULL, &interval);
-        if((activity < 0) && (errno != EINTR)) {
-            cout << "Soket Seçme Hatası!" << endl;
-        }
-
-        if(FD_ISSET(serverSocket, &readfds)) {
-            acceptConnections();
-        }
-
-        handleClients(readfds);
+    if ((serverFd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
     }
 
+    if (setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(serverFd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(serverFd, MAX_CLIENTS) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    while (true) {
+        if ((newSocket = accept(serverFd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            continue;
+        }
+
+        cout << "New connection: " << newSocket << endl;
+
+        clientThreads[newSocket] = make_pair("", thread(handleClient, newSocket));
+    }
+}
+
+int main() {
+    startServer();
     return 0;
 }
